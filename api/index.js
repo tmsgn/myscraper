4f3ae12c5f9638696e37d4bc809017ed875f1f48
Temesgen <tmsgn1996@gmail.com>
2025-10-07 22:04:26 +0300

    vercel: pin Node runtime and export Express app handler; add rewrites

--- api/index.js
+++ api/index.js
@@ -2,5 +2,5 @@ import app from "../app.js";
 
 // Export the Express app directly for Vercel's Node.js runtime
 export default function handler(req, res) {
-	return app(req, res);
+  return app(req, res);
 }
